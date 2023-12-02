2e2be3236cb4e252e1393e3f5160e573ce7f6bce
Gabriela Narvaez <135402790+GabrielaNarvaez28@users.noreply.github.com>
2023-12-02 13:18:56 -0500

    genb pacientes webapp\templates db.sqlite3

--- pacientes/views.py
+++ pacientes/views.py
@@ -2,6 +2,8 @@ from django.forms import modelform_factory
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render, redirect, get_object_or_404
+from openpyxl.workbook import Workbook
+
 from pacientes.forms import PacienteFormulario
 from pacientes.models import Paciente
 
@@ -47,6 +49,41 @@ def eliminar_paciente(request, id):
         paciente.delete()
         return redirect('inicio')
 
+
+def generar_reporte(request, *args, **kwargs):
+    #Obtenemos todas las personas de nuestra base de datos
+    pacientes = Paciente.objects.order_by('apellido', 'nombre')
+    #Creamos el libro de trabajo
+    wb = Workbook()
+    #Definimos como nuestra hoja de trabajo, la hoja activa, por defecto la primera del libro
+    ws = wb.active
+    #En la celda B1 ponemos el texto 'REPORTE DE PERSONAS'
+    ws['B1'] = 'REPORTE DE PACIENTES'
+    #Juntamos las celdas desde la B1 hasta la E1, formando una sola celda
+    ws.merge_cells('B1:E1')
+    #Creamos los encabezados desde la celda B3 hasta la E3
+    ws['B3'] = 'ID'
+    ws['C3'] = 'NOMBRE'
+    ws['D3'] = 'APELLIDO '
+    ws['E3'] = 'CORREO'
+    ws['F3'] = 'SEXO'
+    cont=4
+    #Recorremos el conjunto de personas y vamos escribiendo cada uno de los datos en las celdas
+    for paciente in pacientes:
+        ws.cell(row=cont,column=2).value = paciente.id
+        ws.cell(row=cont,column=3).value = paciente.nombre
+        ws.cell(row=cont,column=4).value = paciente.apellido
+        ws.cell(row=cont,column=5).value = paciente.email
+        ws.cell(row=cont,column=6).value = paciente.sexo
+        cont = cont + 1
+           #Establecemos el nombre del archivo
+        nombre_archivo ="ReportePacientessExcel.xlsx"
+           #Definimos que el tipo de respuesta a devolver es un archivo de microsoft excel
+        response = HttpResponse(content_type="application/ms-excel")
+        contenido = "attachment; filename={0}".format(nombre_archivo)
+    response["Content-Disposition"] = contenido
+    wb.save(response)
+    return response
 
 
 
